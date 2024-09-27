e1000e2b78d1c6a08479a5a3aca84ee23ebad5c1
eider alexis camacho palacios <eider.camacho.1006@miremington.edu.co>
2024-09-26 20:58:12 -0500

    agregar verbo post

--- servern .js
+++ servern .js
@@ -1,7 +1,24 @@
 import express from 'express'
+app.use(express.json())
 
 const app = express()
 const puerto = 3000
+
+let usuarios = [{
+"id":1,
+"nombre": "maria lopez",
+"edad":26,
+}]
+
+app.get('/usuario',(req, res) =>{
+res.json(usuarios)
+})
+
+app.post('/usuarios',(req, res)=>{
+    console.log(req.body)
+});
+const newUser={...req.body, id:usuarios.length +1}
+
 app.listen(puerto,()=>{ 
 console.log(`Hola${puerto}`)
 })
